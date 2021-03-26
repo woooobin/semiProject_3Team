477ba621606f57eb24c16cb6474be08cfdfd1219
WOOBIN HAN <gksdnqls45@naver.com>
2021-03-26 13:03:30 +0900

    0326오전

--- semiProject_3Team/src/com/poosil/login/controller/logincontroller.java
+++ semiProject_3Team/src/com/poosil/login/controller/logincontroller.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
-import javax.servlet.RequestDispatcher;
+import javax.servlet.RequestDispatcher;  
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -39,7 +39,7 @@ public class logincontroller extends HttpServlet {
 			String userid = request.getParameter("userid");
 			String password = request.getParameter("password");
 
-			HttpSession session = request.getSession();
+			HttpSession session = request.getSession();  
 
 			loginDto dto = biz.login(userid, password);
 
