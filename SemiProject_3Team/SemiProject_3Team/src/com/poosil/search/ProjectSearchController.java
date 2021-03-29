b80d011725f5c7b4067bd21134a442bf36e2f288
Journey-han <jjyy0602@gmail.com>
2021-03-30 02:30:12 +0900

    Update ProjectSearchController.java

--- SemiProject_3Team/SemiProject_3Team/src/com/poosil/search/ProjectSearchController.java
+++ SemiProject_3Team/SemiProject_3Team/src/com/poosil/search/ProjectSearchController.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/project.do")
+@WebServlet("/projectsearch.do")
 public class ProjectSearchController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
