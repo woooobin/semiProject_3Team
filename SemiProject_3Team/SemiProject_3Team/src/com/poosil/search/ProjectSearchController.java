acb993b631d66a15acc46897f851a9b246e85f7e
Journey-han <jjyy0602@gmail.com>
2021-04-02 23:53:19 +0900

    지연: 프로젝트 검색 기능 마무리

--- SemiProject_3Team/SemiProject_3Team/src/com/poosil/search/ProjectSearchController.java
+++ SemiProject_3Team/SemiProject_3Team/src/com/poosil/search/ProjectSearchController.java
@@ -13,60 +13,53 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
+
 @WebServlet("/projectsearch.do")
 public class ProjectSearchController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	request.setCharacterEncoding("UTF-8");
-	String command = request.getParameter("command");
-	System.out.println("command : " + command);
-	
-	ProjectSearchDao projectdao = new ProjectSearchDao();
-	
-	// 게시물 리스트
-	if (command.equals("list")) {
-		System.out.println("searchcontroller로 넘어오기");
-		
-		String searchOption = request.getParameter("searchOption");				// 검색 옵션
-		String keyword = request.getParameter("keyword");						// 검색어
-		int currPage = Integer.parseInt(request.getParameter("currPage"));		// 페이지
-		
-		System.out.println("옵션:"+searchOption+"키워드:"+keyword+"현재페이지:"+currPage);
-		
-		// 게시물 카운트
-		int count = projectdao.listCount(searchOption, keyword);
-		System.out.println("글개수:"+count);
-		
-		// 페이지 나누기 처리
-		ProjectPager projectpager = new ProjectPager(count, currPage);
-		
-		// 페이지당 게시물 수
-		int start = projectpager.getPageBegin();
-		int end = projectpager.getPageEnd();
-		
-		List<ProjectSearchDto> list = projectdao.selectList(start, end, searchOption, keyword);
-		
-		// 데이터를 맵에 저장
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("list", list); 					// list
-		map.put("count", count);				// 게시물 갯수
-		map.put("searchOption", searchOption);	// 검색 옵션
-		map.put("keyword", keyword);			// 검색 키워드
-		map.put("projectpager", projectpager);
-		
-		request.setAttribute("map", map);
-		dispatch(request, response, "projectlist.jsp");
-	
-	}
-	
-	
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String command = request.getParameter("command");
+		System.out.println("command : " + command);
+
+		// 게시물 리스트
+		if (command.equals("list")) {
+			ProjectSearchDto projectdto = new ProjectSearchDto();
+			ProjectSearchDao projectdao = new ProjectSearchDao();
+			
+			System.out.println("searchcontroller로 넘어오기");
+
+			String searchOption = request.getParameter("searchOption"); // 검색 옵션
+			System.out.println(searchOption);
+			String keyword = request.getParameter("keyword"); // 검색어
+			System.out.println(keyword);
+
+			System.out.println("옵션:" + searchOption + "키워드:" + keyword);
+
+			List<ProjectSearchDto> list = projectdao.searchList(searchOption, keyword);
+
+			// 데이터를 맵에 저장
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("list", list); // list
+			map.put("searchOption", searchOption); // 검색 옵션
+			map.put("keyword", keyword); // 검색 키워드
+			
+			System.out.println("map : " + map);
+
+			request.setAttribute("map", map);
+			dispatch(request, response, "project_list.jsp");
+
+		} 
+
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
-	
+
 	public void dispatch(HttpServletRequest request, HttpServletResponse response, String url)
 			throws ServletException, IOException {
 
