6fda437d2e029ef87b0ac46e7587eabd739b3a7c
조가린 <41724184+gareen9342@users.noreply.github.com>
2021-04-07 01:13:51 +0900

    .

---
+++ SemiProject_3Team/SemiProject_3Team/src/com/poosil/projects/controller/MainController.java
@@ -0,0 +1,48 @@
+package com.poosil.projects.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class MainController
+ */
+@WebServlet("/main.do")
+public class MainController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public MainController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		
+		
+		HttpSession session = request.getSession();
+		String command = request.getParameter("command");
+		
+		if(command.equals("main")) {
+			
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	protected void dispatch(HttpServletRequest request, HttpServletResponse response, String path)
+			throws ServletException, IOException {
+		RequestDispatcher dispatch = request.getRequestDispatcher(path);
+
+		dispatch.forward(request, response);
+	}
+
+}
---
+++ SemiProject_3Team/SemiProject_3Team/src/com/poosil/projects/controller/RankController.java
@@ -0,0 +1,45 @@
+package com.poosil.projects.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/rank.do")
+public class RankController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public RankController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		
+		
+		HttpSession session = request.getSession();
+		String command = request.getParameter("command");
+		
+		if(command.equals("selectAll")) {
+			
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	protected void dispatch(HttpServletRequest request, HttpServletResponse response, String path)
+			throws ServletException, IOException {
+		RequestDispatcher dispatch = request.getRequestDispatcher(path);
+
+		dispatch.forward(request, response);
+	}
+
+}
